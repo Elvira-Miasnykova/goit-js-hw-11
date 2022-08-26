7f31d7421fd8424d24428cd8ff816d1b29682419
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2022-08-27 01:15:00 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,4 +1,84 @@
 //import './css/styles.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import fetchGallery from './js/fetchGallery';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import PixabayApi from './js/fetchGallery';
+import { galleryItemsMarkup } from './js/galleryItemsMarkup';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    searchInput: document.querySelector('input[name="searchQuery"]'),
+    loadMoreBtn: document.querySelector('.load-more'),
+    galleryEl: document.querySelector('.gallery'),
+}
+
+const PixabayApiService = new PixabayApi();
+//console.log(PixabayApiService);
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.classList.add('is-hidden');
+
+const lightbox = new SimpleLightbox('.photo-card a');
+
+let searchQuery = '';
+
+function onSearch(e) {
+    e.preventDefault();
+    //console.log(e.currentTarget.elements);
+    //PixabayApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+    PixabayApiService.searchQuery = refs.searchInput.value.trim();
+        if (PixabayApiService.searchQuery === "") {
+            Notify.failure("Sorry, this field must be filled");
+            return;
+        }
+    refs.galleryEl.innerHTML = "";
+    PixabayApiService.resetPage();
+    //console.log(PixabayApiService.searchQuery);
+    PixabayApiService.fetchGallery().then(({ data }) => {
+        //console.log(data.hits);
+        if (data.hits.length === 0) {
+            Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            refs.loadMoreBtn.classList.add('is-hidden');
+            return;
+        } else
+            if (data.hits.length <= PixabayApiService.perPage) {
+                Notify.info(`Hooray! We found ${data.totalHits} images.`);
+                appendGalleryItemsMarkup(data.hits);
+                lightbox.refresh();
+                PixabayApiService.incrementPage();
+                refs.loadMoreBtn.classList.add('is-visible');
+                return;
+            }
+    }).catch((error => console.log(error)));
+};
+
+
+function onLoadMore(e) {
+    e.preventDefault();
+    refs.galleryEl.innerHTML = '';
+    PixabayApiService.fetchGallery().then(({ data }) => {
+        const lastImage = data.totalHits - (PixabayApiService.perPage * PixabayApiService.page);
+        appendGalleryItemsMarkup(data.hits);
+        lightbox.refresh();
+        //PixabayApiService.resetPage();
+        PixabayApiService.incrementPage();
+
+        const { height: cardHeight } = refs.galleryEl.firstElementChild.getBoundingClientRect();
+        window.scrollBy({
+            top: cardHeight * 2,
+            behavior: "smooth",
+        });
+        if (lastImage <= 0) {
+            Notify.failure(`We're sorry, but you've reached the end of search results.`);
+            refs.loadMoreBtn.classList.add('is-hidden');
+
+        }
+    }).catch((error) => console.log(error));
+}
+    
+
+function appendGalleryItemsMarkup(images) {
+    refs.galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup(images));
+}
--- src/js/galleryItemsMarkup.js
+++ src/js/galleryItemsMarkup.js
@@ -1,25 +1,29 @@
 // import SimpleLightbox from "simplelightbox";
 // import "simplelightbox/dist/simple-lightbox.min.css";
 
-const galleryItem = document.querySelector('.gallery');
+//const galleryItem = document.querySelector('.gallery');
 
 function galleryItemsMarkup(images) {
     return images.map((image) => {
         return ` <div class="photo-card">
-                    <a href="${image.largeImageURL}">
-                    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+                    <a class="photo-card__link" href="${image.largeImageURL}">
+                    <img class="photo-card__img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
                     </a>
                         <div class="info">
                             <p class="info-item">
+                                <b>Likes</b>
                                 <b>'${image.likes}'</b>
                             </p>
                             <p class="info-item">
+                                <b>Views</b>
                                 <b>'${image.views}'</b>
                             </p>
                             <p class="info-item">
+                                <b>Comments</b>
                                 <b>'${image.comments}'</b>
                             </p>
                             <p class="info-item">
+                                <b>Downloads</b>
                                 <b>'${image.downloads}'</b>
                             </p>
                         </div>
@@ -27,9 +31,9 @@ function galleryItemsMarkup(images) {
     }).join("");
 }
 
-galleryItem.insertAdjacentHTML('beforeend', galleryItemsMarkup(images));
+//galleryItem.insertAdjacentHTML('beforeend', galleryItemsMarkup(images));
 
-export { galleryItem };
+export { galleryItemsMarkup };
 
 // webformatURL - ссылка на маленькое изображение для списка карточек.
 // largeImageURL - ссылка на большое изображение.
